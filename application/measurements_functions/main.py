45a989b1c0ca62958e7eeb40a5dbb7b2079f4cd0
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-11-28 10:58:13 +0100

    Move read_dht_values to main.py

--- application/measurements_functions/main.py
+++ application/measurements_functions/main.py
@@ -1,9 +1,17 @@
-import RPi.GPIO as GPIO
-
 import time
 
-from .measurement_temperature_humidity import read_dht_values
+import RPi.GPIO as GPIO
+import Adafruit_DHT as dht
 
+"""
+    After using functions from this file
+
+    git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+    cd Adafruit_Python_DHT
+    sudo apt-get update
+    sudo apt-get install build-essential python-dev
+    sudo python setup.py install
+"""
 # Ustawienia pinu
 SENSOR_DATA_PIN = 4
 
@@ -35,6 +43,31 @@ def interruptChange(channel):
             flag = 2
             if pwmHighEndTicks > pwmHighStartTicks:
                 pwmHighVal = pwmHighEndTicks - pwmHighStartTicks
+
+
+def read_dht_values(*pins: int) -> dict[str, int] | None:
+    """
+    Returns a dictionary in the following format: {humidity_1: 49, temperature_1: 23}
+    The number of humidities and temperatures depends on the number of pins provided.
+
+    Parameters
+    ----------
+    *pins : int
+    Include as many pin numbers as you have DHT sensors.
+    """
+    measurements = {}
+
+    for pin in pins:
+        humidity, temperature = dht.read_retry(dht.DHT22, pin)
+
+        if humidity is not None and temperature is not None:
+            measurements[f"humidity_{pin}"] = humidity
+            measurements[f"temperature_{pin}"] = temperature
+        else:
+            print("Failed to get reading. Try again!")
+            return
+
+    return measurements
 
 
 # Dodanie obsługi przerwania na danym kanale
